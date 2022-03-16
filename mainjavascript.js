ecceda410988324275db52e3269e5c55b6655c34
Kiran Sanubala <83561712+Niravthebest@users.noreply.github.com>
2022-03-15 19:53:00 -0500

    Add files via upload

---
+++ mainjavascript.js
@@ -0,0 +1,24 @@
+console.log("My First Console Project");
+
+            var first_name = "Nirav ";
+            var last_name = "Sanubala";
+
+            var full_name =  first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var add = a + b;
+            console.log(add);
+
+            var sub = a - b;
+            console.log(sub);
+
+            var multiple = a * b;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
+
+           
